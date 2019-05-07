20de0b1195bb5f7f294e339336b9b07675e0d0be
fuverdred <fred.cook@bristol.ac.uk>
2019-05-07 23:30:30 +0100

    Added many methods to the Position class, Grid class now requires a dictionary

--- Grid_class.py
+++ Grid_class.py
@@ -1,14 +1,16 @@
 import numpy as np
+import re
 
 class Puzzle():
-    def __init__(self, raw):
+    def __init__(self, raw, dic):
         self.raw = raw
+        self.dic = dic #  Large dictionary
         self.size = int(np.sqrt(len(raw)))
-        self.divider = '#'
+        self.divider = '#' #  Represents dark squares on the grid
         assert np.isclose(np.sqrt(len(raw)), self.size) #  Check grid is square
         self.grid = np.array([i for i in raw]).reshape((self.size, -1))
         self.positions = self.get_positions()
-        self.unfilled = self.positions
+        self.unfilled = self.positions[:]
         self.number_positions()
         self.get_crossers()
 
@@ -31,11 +33,11 @@ class Puzzle():
             across = parse_string(''.join(row))
             if across:
                 for index, length in across:
-                    positions.append(Position(i, index, length, 'a'))
+                    positions.append(Position(self, i, index, length, 'a'))
             down = parse_string(''.join(self.grid[:,i]))
             if down:
                 for index, length in down:
-                    positions.append(Position(index, i, length, 'd'))
+                    positions.append(Position(self, index, i, length, 'd'))
         return sorted(positions, key = lambda pos: (pos.i, pos.j))
 
     def number_positions(self):
@@ -102,21 +104,54 @@ class Puzzle():
         print("\\end{Puzzle}")
         
 class Position():
-    def __init__(self, i, j, length, direction):
+    '''
+    A position is a space for a word in the grid, eg. 1 across
+    '''
+    def __init__(self, puzzle, i, j, length, direction):
+        self.puzzle = puzzle #  Puzzle() which contains this position
         self.i = i
         self.j = j
         self.length = length
         self.direction = direction
         self.get_slice() #  Store positions as a numpy slice for easy access
-        self.crossers = []
+        self.crossers = [] #  List of positions which intersect with this one
         self.filled = False
         self.number = None #  Clue number for printing the grid
+        self.get_cell_coords()
+        self.cells = [] #  For storing the cells in the GUI
+        self.update() #  Initialise other values
 
     def get_slice(self):
+        '''The slice of numpy array which forms this position'''
         if self.direction == 'a':
             self.slice = np.s_[self.i, self.j:self.j+self.length]
         else:
             self.slice = np.s_[self.i:self.i+self.length, self.j]
+
+    def get_cell_coords(self):
+        '''A list of grid coords which make up this position'''
+        coord_grid = np.array([[(i,j) for j in range(self.puzzle.size)]
+                               for i in range(self.puzzle.size)])
+        self.coords = [(i,j) for i,j in coord_grid[self.slice]]
+
+    def get_pattern(self):
+        '''Return a regex of the current letters in the position'''
+        return re.compile(''.join([c if c.isalpha() else '.'
+                                   for c in self.puzzle.grid[self.slice]]))
+
+    def get_possible_words(self):
+        '''Return a list of words which fit in this position'''
+        return [word for word in self.puzzle.dic[self.length]
+                if re.match(self.pattern, word)]
+
+    def update(self):
+        self.pattern = self.get_pattern()
+        if all([c.isalpha() for c in self.pattern.pattern]):
+            self.filled = True
+            return
+        self.filled = False #  If a letter has been deleted
+        self.possible_words = self.get_possible_words()
+        self.freedom = len(self.possible_words)
 
     def __repr__(self):
         return str((self.i, self.j, self.length, self.direction))
