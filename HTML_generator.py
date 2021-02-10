3a9b003fd679b7fededfe11048b036f9b7a778bc
fuverdred <fred.cook@bristol.ac.uk>
2021-02-10 17:12:17 +0000

    Made HTML grid script output a string for saving instead of printing output

--- HTML_generator.py
+++ HTML_generator.py
@@ -14,14 +14,16 @@ DARK = '<td><div class="xw_dark"></div></td>'
 translator = {' ': LIGHT,
               '#': DARK}
 
-def grid_row(row, indent=0):
-    print(' '*indent + '<tr>')
-    for char in row:
-        print(' '*(indent+2) + translator[char])
-    print(' '*indent + '</tr>')
+def grid_row(raw, indent=0):
+    row = [' '*indent + '<tr>']
+    for char in raw:
+        row.append(' '*(indent+2) + translator[char])
+    row.append(' '*indent + '</tr>')
+    return '\n'.join(row)
 
 def print_html_grid(grid):
-    print('<table class="xw_grid">')
+    grid_html = ['<table class="xw_grid">']
     for i in range(0, len(grid), int(len(grid)**0.5)):
-        grid_row(grid[i:i+15], indent=2)
-    print('</table>')
+        grid_html.append(grid_row(grid[i:i+15], indent=2))
+    grid_html.append(('</table>'))
+    return '\n'.join(grid_html)
